0cb3814bf80c51146e5b32a09061d5b3e8941ba4
firatsahn <firatsahnn@gmail.com>
2023-09-30 11:02:27 +0300

    new features added

--- README.md
+++ README.md
@@ -1,31 +1,33 @@
 # AVSpeechDownloader
-Simple python script for downloading AVSpeech Dataset. 
 
-It downloads only the relevant part of the video and saves it in a mp4 container.
-
-Tries to download 720p/360p videos with 25fps and audio at 44.1kHz. 
-
-Files names are <yt_id>\_<start_time>\_<end_time>.mp4
-
-Assumptions/Limitations: 
-  - `avspeech_train.csv` and `avspeech_test.csv` are in the same directory as the download.py script.
-  - creates the output folder in the currect directory based on the train/test set. For now you have to change the directories in the code if required.
-  - the script creates a file called `badfiles_train.txt` which lists the youtube id's of the deleted/private videos which are no longer available for download.
+- This script for downloading AVSpeech Dataset. 
+- It downloads only the relevant part of the video and audio. 
+- The script creates a new folder named 'data' under the working directory.
+- Each downloaded audio and video is placed in a folder named after the video id
+  - Name of folder `yt_id`
+  - Name of video and audio files `yt_id\_start_time\_end_time.mp4/mp3`
+  - The script creates a file called `bad_files_csv.txt` which lists the youtube id's of the deleted/private videos which are no longer available for download.
   
 Usage:
   ```
-  python downloader.py train
+  pip install -r requirements.txt
+  python downloader.py --csv ./example.csv
   ```
-Replace train with test if you want to download the test set.
 
-Dependencies:
-```
-  conda install -c conda-forge ffmpeg
-  pip install -U yt-dlp
-  pip install ffmpeg-python
-```
-NOTE:
+Features:
 
-  This is a very simple script. I haven't take too much care, just wrote it quickly to download the dataset. Let me know if you have any problems using it, will try to fix it. Feel free to send in pull requests if you would like to improve it.
-  
-  Cheers!
+| Features        | Required | Values    |
+|-----------------|----------|--------------------|
+| --csv           |    ✅    | csv path           |
+| --format        |     ❌    | best,worst,22/18,137/140   |
+| --audio         |      ❌    | True/False   |
+ 
+
+ 'best'    = The best quality of video
+ 'worst'   = The worst quality of video
+ '22/18'   = 720p 
+ '137/140' =  1080p
+
+NOTE : 
+You can change the number of threads according to your resource constraints.
+Happy downloading!
--- downloader.py
+++ downloader.py
@@ -1,75 +1,112 @@
 import sys
 import os
+import subprocess
 from multiprocessing.pool import ThreadPool
-
 from yt_dlp import YoutubeDL
-import ffmpeg
-
 
 class VidInfo:
-    def __init__(self, yt_id, start_time, end_time, outdir):
+    def __init__(self, yt_id, start_time, end_time, outdir, download_audio=False, video_format='best'):
         self.yt_id = yt_id
         self.start_time = float(start_time)
         self.end_time = float(end_time)
-        self.out_filename = os.path.join(outdir, yt_id + '_' + start_time + '_' + end_time + '.mp4')
+        self.outdir = os.path.join(outdir, str(yt_id))
+        self.video_out_filename = os.path.join(self.outdir, f"{yt_id}_{start_time}_{end_time}_video.mp4")
+        self.audio_out_filename = os.path.join(self.outdir, f"{yt_id}_{start_time}_{end_time}_audio.mp3")
+        self.download_audio = download_audio
+        self.video_format = video_format
+        
+    
 
+    def create_outdir(self):
+        os.makedirs(self.outdir, exist_ok=True)
 
 def download(vidinfo):
-
     yt_base_url = 'https://www.youtube.com/watch?v='
-
-    yt_url = yt_base_url+vidinfo.yt_id
+    yt_url = yt_base_url + vidinfo.yt_id
 
     ydl_opts = {
-        'format': '22/18',
+        'format': vidinfo.video_format,
         'quiet': True,
         'ignoreerrors': True,
         'no_warnings': True,
     }
+
+    if vidinfo.download_audio:
+        ydl_opts['postprocessors'] = [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }]
+
     try:
         with YoutubeDL(ydl_opts) as ydl:
-            download_url = ydl.extract_info(url=yt_url, download=False)['url']
+            info = ydl.extract_info(url=yt_url, download=False)
+            video_url = info['url']
+            video_filename = info['title']
     except:
-        return_msg = '{}, ERROR (youtube)!'.format(vidinfo.yt_id)
+        return_msg = f'{vidinfo.yt_id}, ERROR (youtube)!'
         return return_msg
+
     try:
-        (
-            ffmpeg
-                .input(download_url, ss=vidinfo.start_time, to=vidinfo.end_time)
-                .output(vidinfo.out_filename, format='mp4', r=25, vcodec='libx264',
-                        crf=18, preset='veryfast', pix_fmt='yuv420p', acodec='aac', audio_bitrate=128000,
-                        strict='experimental')
-                .global_args('-y')
-                .global_args('-loglevel', 'error')
-                .run()
+        subprocess.run([
+            'ffmpeg', '-ss', str(vidinfo.start_time), '-to', str(vidinfo.end_time),
+            '-i', video_url, '-c:v', 'libx264', '-crf', '18',
+            '-preset', 'veryfast', '-pix_fmt', 'yuv420p',
+            '-y', vidinfo.video_out_filename
+        ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
 
-        )
-    except:
-        return_msg = '{}, ERROR (ffmpeg)!'.format(vidinfo.yt_id)
+        if vidinfo.download_audio:
+            subprocess.run([
+                'ffmpeg', '-ss', str(vidinfo.start_time), '-to', str(vidinfo.end_time),
+                '-i', video_url, '-q:a', '0', '-map', 'a',
+                '-y', vidinfo.audio_out_filename
+            ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+
+    except subprocess.CalledProcessError:
+        return_msg = f'{vidinfo.yt_id}, ERROR (ffmpeg)!'
         return return_msg
 
-    return '{}, DONE!'.format(vidinfo.yt_id)
-
+    return_msg = f'{vidinfo.yt_id}, DONE!'
+    return return_msg
 
 if __name__ == '__main__':
 
-    split = sys.argv[1]
-    csv_file = 'avspeech_{}.csv'.format(split)
-    out_dir = split
+    if "--csv" not in sys.argv:
+        print("csv path was not found")
+        sys.exit(1)
+    
+    csv_file = sys.argv[sys.argv.index("--csv")+1]
+    if not os.path.exists(csv_file):
+        print("csv path error")
+        sys.exit(1)
 
-    os.makedirs(out_dir, exist_ok=True)
 
+
+    
+    download_audio = "--audio" in sys.argv
+
+    video_format = sys.argv[sys.argv.index("--format") + 1] if "--format" in sys.argv else 'best'
+
+     
+
+    os.makedirs('data', exist_ok=True)
+    
     with open(csv_file, 'r') as f:
         lines = f.readlines()
         lines = [x.split(',') for x in lines]
-        vidinfos = [VidInfo(x[0], x[1], x[2], out_dir) for x in lines]
+        vidinfos = [VidInfo(x[0], x[1], x[2], 'data', download_audio, video_format) for x in lines]
+        for vidinfo in vidinfos:
+            vidinfo.create_outdir()
 
-    bad_files = open('bad_files_{}.txt'.format(split), 'w')
-    results = ThreadPool(5).imap_unordered(download, vidinfos)
-    cnt = 0
+    bad_files = open(f'bad_files_csv.txt', 'w')
+    results = ThreadPool(10).imap_unordered(download, vidinfos) 
+
+    cnt,err_cnt = 0,0
     for r in results:
         cnt += 1
         print(cnt, '/', len(vidinfos), r)
         if 'ERROR' in r:
             bad_files.write(r + '\n')
+            err_cnt += 1
     bad_files.close()
+    print("Total Error : ",err_cnt)
